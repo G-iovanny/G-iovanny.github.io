5a56436b63d7a528f73973e9e29a4f3436f704c8
Giovanny <diasgiovanny@gmail.com>
2023-04-27 05:25:41 +0200

    Published

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -41,13 +41,13 @@ function Carousel({ pictures }) {
         <div style={ sliderStyles }>
             <div style={ numberPictures } className="carousel__counter">{`${currentIndex + 1}/${pictures.length}`}</div>
             <div style={ verifyLeftArrow } onClick={goToPrevious}>
-                <svg width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="48" height="50" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill="white"/>
                 </svg>
 
             </div>
             <div style={ verifyRightArrow } onClick={goToNext}>
-                <svg width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="48" height="50" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" fill="white"/>
                 </svg>
             </div>
